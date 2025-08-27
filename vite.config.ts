322c875dc7de3c03895b1b4c28de3565155d7e53
Stefany Harma <stefanieharma@gmail.com>
2025-08-27 19:39:09 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base: '/quiz-project1/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
->>>>>>> 5395c3d (Initial commit)
 })
